3b5d354627e7542b6ea4a614bef00598e438c9a3
Ran koli <rankoli3@gmail.com>
2018-04-28 14:41:43 +0300

    Add expense sumary to dashboard

--- src/pages/ExpenseDashboardPage.js
+++ src/pages/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpenseList from '../components/ExpenseList';
 import ExpenseListFilters from '../components/ExpenseListFilters';
+import ExpensesSumary from '../components/ExpensesSumary';
 
 const ExpenseDashboardPage = () => (
   <div>
    <ExpenseList />
+   <ExpensesSumary />
    <ExpenseListFilters />
   </div>
 );
